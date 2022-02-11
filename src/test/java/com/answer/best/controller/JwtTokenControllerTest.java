82422d4acfc815c466f26565b57a09a0292d6a1a
karthik raja <karthik18022@gmail.com>
2022-02-11 19:49:12 +0530

    add junit in add/get question

--- src/test/java/com/answer/best/controller/JwtTokenControllerTest.java
+++ src/test/java/com/answer/best/controller/JwtTokenControllerTest.java
@@ -1,9 +1,10 @@
-package com.answer.best;
+package com.answer.best.controller;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,11 +21,7 @@ import com.answer.best.dao.UserService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class BestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+public class JwtTokenControllerTest {
 	
 	@MockBean
 	UserService userService;
@@ -45,6 +42,5 @@ class BestApplicationTests {
 		 assertNotNull(token);
 	        mvc.perform(MockMvcRequestBuilders.get("/user/answers").header("Authorization", token)).andExpect(status().isOk());
 	}
-
 
 }
--- src/test/java/com/answer/best/controller/QuestionControllerTest.java
+++ src/test/java/com/answer/best/controller/QuestionControllerTest.java
@@ -1,15 +1,19 @@
 package com.answer.best.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,6 +29,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -70,14 +75,26 @@ public class QuestionControllerTest {
 	  }
 	
 	@Test
-	public void test() {
-		List<QuestionResponse> questionList=questionImpl.getQuestions();
-		assertNotNull(questionList);
+	public void test() throws Exception {
+		List<QuestionResponse> list=new ArrayList<>();
+		QuestionResponse question=new QuestionResponse();
+		question.setQuestionId(11);
+		question.setQuestion("test");
+		question.setOptionA("test-1");
+		question.setOptionB("test-2");
+		question.setOptionC("test-3");
+		question.setOptionD("test-4");
+		list.add(question);
+		  Mockito.when(questionImpl.getQuestions()).thenReturn(list);
+		  mvc.perform(get("/questions")).andExpect(status().isOk()).andExpect(jsonPath("$", Matchers.aMapWithSize(4)));
+//          .andExpect(jsonPath("$.response", Matchers.equalTo("questionId=11,question=test,optionA=test-1,optionB=test-2,optionC=test-3,optionD=test-4")));
+//		List<QuestionResponse> questionList=questionImpl.getQuestions();
+//		assertNotNull(questionList);
 		}
 	
 	@Test
 	public void addQuestion() throws Exception {
-		String url = "http://localhost:8080" + "/add/question";
+		String url = "http://localhost:8080" + "/question";
 		Questions question=new Questions();
 		question.setQuestionId(11);
 		question.setQuestion("test");
@@ -96,11 +113,12 @@ public class QuestionControllerTest {
 	            .andReturn();
 	    Assert.assertNotNull(result);
 //	    Mockito.verify(questionImpl,Mockito.times(1)).addQuestion();
-	   assertThat(question);
+	 
 	}
 	
 	@Test
-	public void addAnswer() {
+	public void addAnswer() throws Exception {
+		String url = "http://localhost:8080" + "/userAnswer";
 		RequestVO req=new RequestVO();
 		req.setEmail("test@gmail.com");
 		req.setPassword("test");
@@ -117,8 +135,37 @@ public class QuestionControllerTest {
 	    list.add(qRequest);
 	    req.setRequest(list);
 	    req.setUsername("test");
-	    answerImpl.postAnswer(req);
-	    Mockito.verify(answerImpl,Mockito.times(1)).postAnswer(req);
-	    Assert.assertNotNull(req);
+	    com.google.gson.Gson gson=new com.google.gson.Gson();
+		 String json=gson.toJson(req);
+		 MvcResult result = mvc.perform(
+		            post(url)
+		            .contentType(MediaType.APPLICATION_JSON)
+		            .content(json))
+		            .andExpect(status().isOk())
+		            .andReturn();
+		    Assert.assertNotNull(result);
+//	    answerImpl.postAnswer(req);
+//	    Mockito.verify(answerImpl,Mockito.times(1)).postAnswer(req);
+//	    Assert.assertNotNull(req);
+	}
+	
+	@Test
+	public void existentUserCanGetTokenAndAuthentication() throws Exception {
+	    String email = "kanimozhi@gmail.com";
+	    String password = "password";
+
+	    String body = "{\"email\":\"" + email + "\", \"password\":\""+ password + "\"}";
+
+	    MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/authenticate").contentType(MediaType.APPLICATION_JSON)
+	            .content(body))
+	            .andExpect(status().isOk()).andReturn();
+
+	    String response = result.getResponse().getContentAsString();
+	    response = response.replace("{\"jwttoken\": \"", "");
+	    String token = response.replace("\"}", "");
+
+	    mvc.perform(MockMvcRequestBuilders.get("/user/answers")
+	        .header("Authorization", "Bearer " + token))
+	        .andExpect(status().isOk());
 	}
 }
